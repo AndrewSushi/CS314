601b4007ddf13153f1192035304ce83408f8d695
AndrewSushi <88579779+AndrewSushi@users.noreply.github.com>
2022-09-29 00:45:55 -0700

    Update 1-3.c

--- Assignment1/1-3.c
+++ Assignment1/1-3.c
@@ -3,7 +3,7 @@
 int oddBit(unsigned int x){
 	unsigned int mask = 0xAAAAAAAA; // Create a mask of 101010... that is 32 bits long
 	x &= mask; // And the x value and the mask together, if the value of x is greater than 0, then there is a 1 at an odd index, 
-               //if not then there is none
+               	   //if not then there is none
 	return x && x; // If true, reduce the number to 1 (true) by logical AND with itself, otherwise 0 And 0 will result 0
 }
 
