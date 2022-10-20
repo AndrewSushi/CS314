8e515831c848251308e7a389e1bbb75543d8d18f
Andrew Chan <88579779+AndrewSushi@users.noreply.github.com>
2022-10-19 21:26:05 -0700

    Add files via upload

--- Assignment3/3-1.c
+++ Assignment3/3-1.c
@@ -9,6 +9,7 @@ struct IntArray{
 struct IntArray* mallocIntArray(int length){
 	struct IntArray *arr = (struct IntArray *)malloc(sizeof(struct IntArray));
 	arr->dataPtr = (int *)malloc(sizeof(int) * length);
+	arr->length = length;
 	return arr;
 }
 
@@ -18,14 +19,73 @@ void freeIntArray(struct IntArray *arrayPtr){
 }
 
 void readIntArray(struct IntArray *array){
-	char arr[array->length];
+	char lineBuf[10];
+	char *p = NULL;
+	int n;
 	for(int i = 0; i < array->length; i++){
 		while(1){
-			fgets(arr, sizeof(arr), stdin);
+			printf("Enter int: ");
+			fgets(lineBuf, sizeof(lineBuf), stdin);
+			n = strtol(lineBuf, &p, 10);
+			if(lineBuf != p){
+				break;
+			}
+			printf("Invalid Input\n");
+		}
+		array->dataPtr[i] = n;
+	}
+}
+
+void swap(int *xp, int *yp){
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+void sortIntArray(struct IntArray *array){
+	for(int i = 0; i < array->length; i++){
+		int switcher = 1;
+		for(int j = 0; j < array->length - 1; j++){
+			if(array->dataPtr[j] > array->dataPtr[j+1]){
+				swap(&array->dataPtr[j], &array->dataPtr[j+1]);
+				switcher = 0;
+			}
+		}
+		if(switcher){
+			break;
 		}
 	}
 }
 
+void printArray(struct IntArray *array){
+	printf("[");
+	for(int i = 0; i < array->length; i++){
+		if(i < array->length - 1){
+			printf("%d, ", array->dataPtr[i]);
+			continue;
+		}
+		printf("%d", array->dataPtr[i]);
+	}
+	printf("]\n");
+}
+
 int main(){
-	
+	int length;
+	char lineBuf[10];
+	char *p = NULL;
+	while(1){
+		printf("Enter length: ");
+		fgets(lineBuf, sizeof(lineBuf), stdin);
+		length = strtol(lineBuf, &p, 10);
+		if(lineBuf != p){
+			break;
+		}
+		printf("Invalid Input\n");
+	}
+	struct IntArray *head = mallocIntArray(length);
+	readIntArray(head);
+	sortIntArray(head);
+	printArray(head);
+	freeIntArray(head);
+
 }
