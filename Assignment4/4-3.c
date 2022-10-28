06adc57e60b445c603e52ebf0109387b93986797
Andrew Chan <88579779+AndrewSushi@users.noreply.github.com>
2022-10-27 18:32:13 -0700

    Add files via upload

--- Assignment4/4-3.c
+++ Assignment4/4-3.c
@@ -1,6 +1,9 @@
+#include <stdio.h>
+
 #define N 4
 typedef long array_t[N][N];
 
+/*
 void transpose(array_t a) {
     for (long i = 0; i < N; ++i) {
         for (long j = 0; j < i; ++j) {
@@ -10,4 +13,29 @@ void transpose(array_t a) {
             a[j][i] = t1;
         }
     }
+}
+*/
+
+void transpose(array_t a){
+	for(long i = 0; i < N; ++i){
+		for(long j = 0; j < i; ++j){
+			
+		}
+	}
+}
+
+// .L3: for(long i = 0; i < N ; ++i)
+// movq (%rax), %rcx 
+// movq (%rdx), %rsi
+// movq %rsi, (%rax)
+// movq %rcx, (%rdx)
+// addq $8, %rax
+// addq $32, %rdx
+// cmpq %r9, %rax
+// jne .L3
+
+int main(){
+	array_t a = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15,
+16}};
+	return 0;
 }
